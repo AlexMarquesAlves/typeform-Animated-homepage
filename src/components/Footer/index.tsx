3964a6bad6f7de02588c03d7940b2160f5a8b7f4
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-14 10:24:39 -0300

    Ending Component Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,11 @@
-import { ReactNode } from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-interface FooterProps {
-  children: ReactNode;
-}
-
-function Footer({ children }: FooterProps) {
+const Footer: React.FC = () => {
   return (
     <Container>
       <h1>Footer</h1>
-      {children}
     </Container>
   );
 };
